c121341e20deffab2bb4821cf6b89773425fb9c9
iraklikeche <irakli.kechee@gmail.com>
2024-05-31 19:16:12 +0400

    feat:complete-tests

--- app/Listeners/SendQuoteLikedNotification.php
+++ app/Listeners/SendQuoteLikedNotification.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\QuoteLiked;
 use App\Notifications\QuoteLikedNotification;
+use Illuminate\Support\Facades\Log;
 
 class SendQuoteLikedNotification
 {
@@ -17,6 +18,15 @@ class SendQuoteLikedNotification
 
     public function handle(QuoteLiked $event)
     {
-        $event->quote->user->notify(new QuoteLikedNotification($event->quote, $event->user));
+        // $event->quote->user->notify(new QuoteLikedNotification($event->quote, $event->user));
+        Log::info('Handling QuoteLiked event', ['quote_id' => $event->quote->id, 'user_id' => $event->user->id]);
+
+        // Check if the user is being notified
+        if ($event->quote->user) {
+            $event->quote->user->notify(new QuoteLikedNotification($event->quote, $event->user));
+            Log::info('QuoteLikedNotification sent', ['quote_id' => $event->quote->id, 'user_id' => $event->user->id]);
+        } else {
+            Log::error('QuoteLikedNotification not sent - no user found', ['quote_id' => $event->quote->id]);
+        }
     }
 }
