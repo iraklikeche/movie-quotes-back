90f9440f15df1b7d46a85ea6452685a26a3d90f0
iraklikeche <irakli.kechee@gmail.com>
2024-06-01 14:26:11 +0400

    fix:language-grammar

--- lang/ka/reactions.php
+++ lang/ka/reactions.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'reacted_to_your_quote' => 'თქვენი ციტატის რეაქცია',
+    'reacted_to_your_quote' => 'გამოხატა რეაქცია თქვენს ციტატაზე',
     'commented_to_your_movie_quote' => 'დატოვა კომენტარი თქვენს ციტატაზე',
 
 ];
