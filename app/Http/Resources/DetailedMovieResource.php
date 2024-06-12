d15c589684600d2dd383eb98752fb5c8c2c1dae1
iraklikeche <irakli.kechee@gmail.com>
2024-06-12 14:56:21 +0400

    refactor:remove-comments

--- app/Http/Resources/DetailedMovieResource.php
+++ app/Http/Resources/DetailedMovieResource.php
@@ -9,32 +9,7 @@ class DetailedMovieResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        // $movieNameEn = $this->getTranslation('name', 'en');
-        // $movieNameKa = $this->getTranslation('name', 'ka');
-        // $directorEn = $this->getTranslation('director', 'en');
-        // $directorKa = $this->getTranslation('director', 'ka');
-        // $descriptionEn = $this->getTranslation('description', 'en');
-        // $descriptionKa = $this->getTranslation('description', 'ka');
-        // $imageUrl = $this->media->first()?->getUrl() ?? null;
-
-        // return [
-        //     'id' => $this->id,
-        //     'movie_name' => [
-        //         'en' => $movieNameEn,
-        //         'ka' => $movieNameKa,
-        //     ],
-        //     'year' => $this->year,
-        //     'image_url' => $imageUrl,
-        //     'director' => [
-        //         'en' => $directorEn,
-        //         'ka' => $directorKa,
-        //     ],
-        //     'description' => [
-        //         'en' => $descriptionEn,
-        //         'ka' => $descriptionKa,
-        //     ],
-        //     'genres' => GenreResource::collection($this->genres),
-        // ];
+    
         $nameTranslations = $this->getTranslations('name');
         $directorTranslations = $this->getTranslations('director');
         $descriptionTranslations = $this->getTranslations('description');
