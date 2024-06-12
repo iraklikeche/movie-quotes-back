1f93da838da78b636a23d6c40d66d7530433ddb5
iraklikeche <irakli.kechee@gmail.com>
2024-06-12 11:20:20 +0400

    fix:validation-for-quotes

--- app/Http/Requests/StoreQuoteRequest.php
+++ app/Http/Requests/StoreQuoteRequest.php
@@ -10,14 +10,16 @@ class StoreQuoteRequest extends FormRequest
     {
         return [
             'content.en' => [
-                'required', 
-                'string', 
-                'regex:/^[a-zA-Z0-9\s,.!\-?]+$/'
+                'required',
+                'string',
+                'regex:/^[a-zA-Z0-9\s,.!\-?\'"]+$/'
+
             ],
             'content.ka' => [
-                'required', 
-                'string', 
-                'regex:/^[ა-ჰ0-9\s,.!\-?]+$/'
+                'required',
+                'string',
+                'regex:/^[ა-ჰ0-9\s,.!\-?\'"]+$/'
+
             ],
             'movie_id' => 'required|exists:movies,id',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
